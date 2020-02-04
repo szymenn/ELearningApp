820e27907a6685cddc398b5697fe05e31c5977db
szymenn <ilikedzimi@gmail.com>
2020-02-04 18:50:46 +0100

    register database contexts and asp net identity

--- ELearningApp.Api/Startup.cs
+++ ELearningApp.Api/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ELearningApp.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +28,19 @@ namespace ELearningApp.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UserStoreDbContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("UserStore")));
+            services.AddDbContext<TokenStoreDbContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("TokenStore")));
+            
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+                {
+                    options.SignIn.RequireConfirmedEmail = true;
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<UserStoreDbContext>()
+                .AddDefaultTokenProviders();
+            
             services.AddControllers();
         }
 
--- ELearningApp.Core/Entities/Auth/AppUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace ELearningApp.Core.Entities.Auth
-{
-    public class AppUser : IdentityUser
-    {
-        
-    }
-}
