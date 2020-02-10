c57ada686085d30c9e161895370cf2e704970df4
szymenn <ilikedzimi@gmail.com>
2020-02-10 13:46:54 +0100

    implement UserRepository class

--- ELearningApp.Infrastructure/Repositories/Auth/UserRepository.cs
+++ ELearningApp.Infrastructure/Repositories/Auth/UserRepository.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using ELearningApp.Core.Dtos.ApiModels.Auth;
 using ELearningApp.Core.Dtos.InputModels.Auth;
+using ELearningApp.Core.Exceptions;
+using ELearningApp.Core.Helpers;
 using ELearningApp.Core.Interfaces.Repositories.Auth;
 using ELearningApp.Core.Interfaces.Services.Auth;
 using Microsoft.AspNetCore.Identity;
@@ -9,10 +13,10 @@ namespace ELearningApp.Infrastructure.Repositories.Auth
 {
     public class UserRepository : IUserRepository
     {
-        private UserManager<IdentityUser> _userManager;
-        private ITokenService _tokenService;
-        private IEmailService _emailService;
-        private SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ITokenService _tokenService;
+        private readonly IEmailService _emailService;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
         public UserRepository(
             UserManager<IdentityUser> userManager,
@@ -28,19 +32,54 @@ namespace ELearningApp.Infrastructure.Repositories.Auth
         }
         
         
-        public Task<EmailResponse> Register(RegisterInputModel registerModel)
+        public async Task<EmailResponse> Register(IdentityUser user, string password)
         {
-            throw new System.NotImplementedException();
+            if (_userManager.Users.Any(u => u.UserName == user.UserName)) 
+            {
+                throw new ResourceAlreadyExistsException(Constants.UserAlreadyExists);
+            }
+            
+            var result = await _userManager.CreateAsync(user, password);
+            if (!result.Succeeded)
+            {
+                throw new RegistrationException(Constants.RegistrationError);
+            }
+            
+            var confirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            return await _emailService.SendEmailAsync(user, confirmationToken);
         }
 
-        public Task<JsonWebToken> Login(LoginInputModel loginModel)
+        public async Task<JsonWebToken> Login(IdentityUser user, string password)
         {
-            throw new System.NotImplementedException();
+            var loginUser = _userManager.Users.FirstOrDefault(u => u.UserName == user.UserName);            
+            if (loginUser == null)
+            {
+                throw new ResourceNotFoundException(Constants.UserNotFound);
+            }
+            
+            var result = await _signInManager.PasswordSignInAsync
+                (loginUser, password, false, false);
+            if (!result.Succeeded)
+            {
+                throw new LoginException(Constants.LoginFailed);
+            }
+
+            return _tokenService.CreateAccessToken(user.UserName, Guid.Parse(user.Id));
         }
 
-        public Task VerifyEmail(string userId, string emailToken)
+        public async Task VerifyEmail(string userId, string emailToken)
         {
-            throw new System.NotImplementedException();
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                throw new ResourceNotFoundException(Constants.UserNotFound);
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, emailToken);
+            if (!result.Succeeded)
+            {
+                throw new EmailVerificationException(Constants.EmailVerificationException);
+            }
         }
     }
 }
